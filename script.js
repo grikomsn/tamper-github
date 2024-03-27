b8027c0d3f66007dcd0290b9fc84aa590588ac7f
Griko Nibras <griko@nibras.co>
2024-03-28 04:48:00 +0700

    Create script.js
    Signed-off-by: Griko Nibras <griko@nibras.co>

---
+++ script.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Tamper GitHub
+// @version      0.0.1
+// @description  A userscript to disable GitHub turbolinks and hide unnecessary elements
+// @license      MIT
+// @author       Griko Nibras
+// @namespace    https://github.com/grikomsn
+// @match        https://github.com/*
+// @match        https://gist.github.com/*
+// @run-at       document-end
+// @grant        none
+// @icon         https://github.githubassets.com/pinned-octocat.svg
+// @updateURL    https://raw.githubusercontent.com/grikomsn/tamper-github/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/grikomsn/tamper-github/main/script.js
+// @supportURL   https://github.com/grikomsn/tamper-github/issues
+// ==/UserScript==
+
+(() => {
+  "use strict";
+  document.body.dataset.turbo = "false";
+  let el = document.createElement("style");
+  el.innerHTML = `.js-repos-container, #dashboard-user-teams { display: none; }`;
+  document.head.appendChild(el);
+})();
