e4f533f7eabb5b6c9bd4288d5d2ac1890125d078
Griko Nibras <griko@nibras.co>
2024-04-23 04:40:45 +0700

    Update script.js
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tamper GitHub
-// @version      0.1.3
+// @version      0.2.0
 // @description  A userscript to disable GitHub turbolinks and toggle elements to blur out
 // @license      MIT
 // @author       Griko Nibras
@@ -22,7 +22,8 @@ var css = String.raw;
 (() => {
   "use strict";
 
-  // document.body.dataset.turbo = "false";
+  document.body.dataset.turbo = "false";
+
   var el = document.createElement("style");
   el.innerHTML += css`
     tool-tip {
@@ -59,31 +60,39 @@ var css = String.raw;
     el.id = id;
     el.innerHTML += css`
       #js-contribution-activity {
-        filter: blur(4px);
+        font-family: "Redacted Script", "Webdings", "Wingdings", "Zapf Dingbats",
+          sans-serif;
       }
     `;
     keywords.forEach((keyword) => {
       el.innerHTML += css`
+        .orghead:has([href*="${keyword}"]) img,
+        .Popover-message:has([href*="${keyword}"]) img,
+        [href*="${keyword}"] img,
+        [href="/orgs/${keyword}/people"] + .flex-wrap *,
+        [data-redact] {
+          background: gray !important;
+          border-radius: 2px !important;
+          filter: grayscale(100%) invert(50%) !important;
+        }
+
         .js-for-you-feed-items > *:has([href*="${keyword}"]),
         .orghead:has([href*="${keyword}"]) h1 + *,
         .orghead:has([href*="${keyword}"]) h1,
-        .orghead:has([href*="${keyword}"]) img,
         .orghead:has([href*="${keyword}"]) ul,
-        .Popover-message div:has([href*="${keyword}"]) ~ .color-fg-muted,
-        .Popover-message:has([href*="${keyword}"]) [itemprop="description"],
+        .Popover-message:has([href*="${keyword}"]),
         [aria-label="Explore"] div:has([href*="${keyword}"]) + p,
         [aria-label="Explore"] span:has([href*="${keyword}"]),
         [href*="${keyword}"] + tool-tip,
         [href*="${keyword}"],
         [href*="${keyword}"]:not([itemprop="programmingLanguage"]),
-        [href="/orgs/${keyword}/people"] + .flex-wrap,
         [itemprop="name codeRepository"][href*="${keyword}"]
           ~ [itemprop="description"],
-        div:has([href*="${keyword}"]) ~ .pinned-item-desc {
-          filter: blur(8px) grayscale(100%);
-        }
-        .orghead:has([href*="${keyword}"]) img {
-          filter: blur(8px) invert(50%);
+        div:has([href*="${keyword}"]) ~ .pinned-item-desc,
+        li:has([href*="${keyword}"]),
+        [data-redact-text] {
+          font-family: "Redacted Script", "Webdings", "Wingdings",
+            "Zapf Dingbats", sans-serif;
         }
       `;
     });
