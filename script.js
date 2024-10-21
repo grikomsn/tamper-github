40d237dc8470d90645c490d5feda88a8221e4edb
Griko Nibras <griko@nibras.co>
2024-10-22 02:44:50 +0700

    Update script.js
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tamper GitHub
-// @version      0.2.0
+// @version      0.3.0
 // @description  A userscript to disable GitHub turbolinks and toggle elements to blur out
 // @license      MIT
 // @author       Griko Nibras
@@ -31,72 +31,4 @@ var css = String.raw;
     }
   `;
   document.head.appendChild(el);
-
-  document.addEventListener("keydown", (event) => {
-    if (event.metaKey && event.ctrlKey && event.shiftKey && event.key === "R") {
-      toggleRedact();
-    }
-  });
-  if (typeof GM_registerMenuCommand === "function") {
-    GM_registerMenuCommand("Toggle Redact", toggleRedact);
-  }
-  if (GM_getValue("lastState", true)) {
-    toggleRedact();
-  }
-
-  function toggleRedact() {
-    var id = "tamper-github-redact-style";
-    var keywords = GM_getValue("keywords", []);
-    if (keywords.length < 1) {
-      console.error("toggleRedact error: no keywords found");
-    }
-    var current = document.getElementById(id);
-    if (current) {
-      current.remove();
-      GM_setValue("lastState", false);
-      return;
-    }
-    var el = document.createElement("style");
-    el.id = id;
-    el.innerHTML += css`
-      #js-contribution-activity {
-        font-family: "Redacted Script", "Webdings", "Wingdings", "Zapf Dingbats",
-          sans-serif;
-      }
-    `;
-    keywords.forEach((keyword) => {
-      el.innerHTML += css`
-        .orghead:has([href*="${keyword}"]) img,
-        .Popover-message:has([href*="${keyword}"]) img,
-        [href*="${keyword}"] img,
-        [href="/orgs/${keyword}/people"] + .flex-wrap *,
-        [data-redact] {
-          background: gray !important;
-          border-radius: 2px !important;
-          filter: grayscale(100%) invert(50%) !important;
-        }
-
-        .js-for-you-feed-items > *:has([href*="${keyword}"]),
-        .orghead:has([href*="${keyword}"]) h1 + *,
-        .orghead:has([href*="${keyword}"]) h1,
-        .orghead:has([href*="${keyword}"]) ul,
-        .Popover-message:has([href*="${keyword}"]),
-        [aria-label="Explore"] div:has([href*="${keyword}"]) + p,
-        [aria-label="Explore"] span:has([href*="${keyword}"]),
-        [href*="${keyword}"] + tool-tip,
-        [href*="${keyword}"],
-        [href*="${keyword}"]:not([itemprop="programmingLanguage"]),
-        [itemprop="name codeRepository"][href*="${keyword}"]
-          ~ [itemprop="description"],
-        div:has([href*="${keyword}"]) ~ .pinned-item-desc,
-        li:has([href*="${keyword}"]),
-        [data-redact-text] {
-          font-family: "Redacted Script", "Webdings", "Wingdings",
-            "Zapf Dingbats", sans-serif;
-        }
-      `;
-    });
-    document.head.appendChild(el);
-    GM_setValue("lastState", true);
-  }
 })();
